db512ccc043a3435282bc9484f314ca84df1b972
Kostya <tolok.kostya@gmail.com>
2022-08-16 13:37:44 +0300

    create model

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+
 from users.models import *
 
-
+admin.site.register(User)
 admin.site.register(UserProfile)
+admin.site.unregister(Group)
--- users/models.py
+++ users/models.py
@@ -1,7 +1,61 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-User = get_user_model()
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, username, password):
+        if not email:
+            raise ValueError('Email must be specified')
+        if not username:
+            raise ValueError('Username must be specified')
+
+        user = self.model(email=email, username=username)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, username, password=None):
+        user = self.create_user(email="admin@gmail.com", username=username, password=password)
+
+        user.is_admin = True
+        user.save(using=self._db)
+
+        UserProfile.objects.create(user=user)
+
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(verbose_name="Email пользователя", max_length=40, unique=True)
+    username = models.CharField(verbose_name="Имя пользователя", max_length=40, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'username']
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    @staticmethod
+    def has_perm(perm, obj=None):
+        return True
+
+    @staticmethod
+    def has_module_perms(app_label):
+        return True
+
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        return self
 
 
 class UserProfile(models.Model):
