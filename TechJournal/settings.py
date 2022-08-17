e1b1e9c6189f1b200c30db2321f8bc39bdaf73d3
Kostya <tolok.kostya@gmail.com>
2022-08-17 15:50:00 +0300

    fix csrf error

--- TechJournal/settings.py
+++ TechJournal/settings.py
@@ -174,5 +174,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = ['https://tech-journal-app.herokuapp.com/']
 
 django_heroku.settings(locals())
